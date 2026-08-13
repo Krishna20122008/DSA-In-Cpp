9e80f912bcab7c18a8d3783714292583504fa2aa
Krishna20122008 <kchourasia121@gmail.com>
2026-08-13 10:40:19 +0530

    Pushed BubbleSort.cpp

---
+++ Sorting Algorithms/BubbleSort.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+using namespace std;
+int main(){
+    vector<int> arr = {5,4,3,2,1};
+    for(int i=0; i<arr.size(); i++){
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    for(int i = 1; i<arr.size(); i++){
+        for(int j=0; j<arr.size() -1; j++){
+            if(arr[j]>arr[j+1]) {
+                swap(arr[j], arr[j+1]);
+            }
+        }
+    }
+
+    for(int i=0; i<arr.size(); i++){
+        cout << arr[i] << " ";
+    }
+}
