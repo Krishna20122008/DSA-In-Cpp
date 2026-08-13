f71ae4926074091e4d1adcd4db98c68465eb015b
Krishna20122008 <kchourasia121@gmail.com>
2026-08-13 20:35:30 +0530

    More optimised BubbleSort.cpp

--- Sorting Algorithms/BubbleSort.cpp
+++ Sorting Algorithms/BubbleSort.cpp
@@ -16,12 +16,22 @@ int main(){
     //         }
     //     }
     // }
-    for(int i = 0; i<arr.size() -1; i++){              // More optimised
+    // for(int i = 0; i<arr.size() -1; i++){              // More optimised
+    //     for(int j=0; j<arr.size() -1 -i; j++){
+    //         if(arr[j]>arr[j+1]) {
+    //             swap(arr[j], arr[j+1]);
+    //         }
+    //     }
+    // }
+    for(int i = 0; i<arr.size() -1; i++){              // Even More optimised
+        int swaps = 0;
         for(int j=0; j<arr.size() -1 -i; j++){
             if(arr[j]>arr[j+1]) {
                 swap(arr[j], arr[j+1]);
+                swaps++;
             }
         }
+        if(swaps == 0) break;
     }
 
     for(int i=0; i<arr.size(); i++){
