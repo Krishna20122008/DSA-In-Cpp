d786dae075d7feb53e28f9056fcad33b61c86273
Krishna20122008 <kchourasia121@gmail.com>
2026-08-17 10:27:17 +0530

    Pushed BinarySearch.cpp

---
+++ Searching/BinarySearch.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+class Solution {
+public:
+    int search(vector<int>& arr, int target) {
+        int lo = 0; 
+        int hi = arr.size() -1;
+
+        while(lo<=hi){
+            int mid = (hi+lo)/2;
+            if(arr[mid]>target) hi=mid-1;
+            else if(arr[mid]<target) lo=mid+1;
+            else return mid;
+        }
+        return -1;
+    }
+};
