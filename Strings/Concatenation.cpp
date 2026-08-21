c597d27f02a8b9c8b0606ed1a58c2d2419902431
Krishna20122008 <kchourasia121@gmail.com>
2026-08-21 20:58:53 +0530

    Pushed Concatenation.cpp

---
+++ Strings/Concatenation.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+    string s1 = "abc";
+    string s2 = "xyz";
+    cout << s1+s2 << endl;
+
+    string s3 = "111";
+    string s4 = "222";
+    cout << s3+s4 << endl;
+}
