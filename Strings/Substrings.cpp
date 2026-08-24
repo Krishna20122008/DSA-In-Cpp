498d9b88d32fc6b9b1dd0201aac71ee3849a369f
Krishna20122008 <kchourasia121@gmail.com>
2026-08-24 20:14:44 +0530

    Pushed Substring.cpp

---
+++ Strings/Substrings.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+    string s = "Krishna";
+    string sub = s.substr(0,4);
+    cout << s << endl;
+    cout << sub << endl;
+}
