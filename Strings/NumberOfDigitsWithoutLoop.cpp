4855053ccd1c169aa6215f732b65ffa0f9968846
Krishna20122008 <kchourasia121@gmail.com>
2026-08-24 09:52:54 +0530

    Fixed Bug

--- Strings/NumberOfDigitsWithoutLoop.cpp
+++ Strings/NumberOfDigitsWithoutLoop.cpp
@@ -4,6 +4,7 @@ int main(){
     int n;
     cout << "Enter a number: ";
     cin >> n;
+    if(n<0) n*=-1;
     string s = to_string(n);
 
     cout << "The number of digits in the entered integer is: " << s.size() << endl;
