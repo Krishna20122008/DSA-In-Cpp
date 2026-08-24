23004e04c65ac0bda87d86d5ad4eebc704e3cdd8
Krishna20122008 <kchourasia121@gmail.com>
2026-08-24 20:32:39 +0530

    Edited Substring.cpp

--- Strings/Substrings.cpp
+++ Strings/Substrings.cpp
@@ -5,5 +5,8 @@ int main(){
     string s = "Krishna";
     string sub = s.substr(0,4);
     cout << s << endl;
-    cout << sub << endl;
+    cout << sub << endl;    // Here when we start from 0 index then it works till n-1 index
+
+    cout << s.substr(1,4) << endl;      // But as soon as we put any other number it works till n index
+    cout << s.substr(4) << endl;        // From 4 to end
 }
