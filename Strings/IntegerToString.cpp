a73661d51502f323c66a81d9498882ab0ac6bcf0
Krishna20122008 <kchourasia121@gmail.com>
2026-08-24 09:42:14 +0530

    Pushed IntegerToString.cpp

---
+++ Strings/IntegerToString.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<string>
+#include<algorithm>
+using namespace std;
+int main(){
+    int n;
+    cout << "Enter an integer: ";
+    cin >> n;
+    string s = "";
+
+    while(n!=0){
+        int lastDigit = n%10;
+        n/=10;
+        char ch = lastDigit+48;
+        s.push_back(ch);
+    }
+
+    reverse(s.begin(), s.end());
+    cout << s << "";
+}
