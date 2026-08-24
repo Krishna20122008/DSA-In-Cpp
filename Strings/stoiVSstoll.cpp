b0c61251ba5c0501b4f2e43da3744cf94bbfb977
Krishna20122008 <kchourasia121@gmail.com>
2026-08-24 10:20:37 +0530

    Pushed stoiVSstoll

---
+++ Strings/stoiVSstoll.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+    string s = "389382";
+    int n1 = stoi(s);
+    long long n2 = stoll(s);
+    cout << n1 << endl;
+    cout << n2 << endl;
+}
