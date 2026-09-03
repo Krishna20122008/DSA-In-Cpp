ce6d2922f6268cdd7db7a23ad10127c02fa6b1b7
Krishna20122008 <kchourasia121@gmail.com>
2026-09-03 21:31:29 +0530

    Pushed gcd.cpp

--- Recursion/gcd.cpp
+++ Recursion/gcd.cpp
@@ -1,9 +1,13 @@
 #include<iostream>
 using namespace std;
+// int hcf(int a, int b){
+//     for(int i = min(a,b); i>0; i--){
+//         if(a%i==0 && b%i==0) return i;       TC: O(n)
+//     }
+// }
 int hcf(int a, int b){
-    for(int i = min(a,b); i>0; i--){
-        if(a%i==0 && b%i==0) return i;
-    }
+    if(a==0) return b;
+    return hcf(b%a, a);
 }
 int main(){
     int a, b;
