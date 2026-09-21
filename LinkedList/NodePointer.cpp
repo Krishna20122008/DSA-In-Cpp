f5f8e83c13c7ba7f76d3d3702604d27af45adf74
Krishna20122008 <kchourasia121@gmail.com>
2026-09-21 09:32:53 +0530

    Pushed NodeClass and NodePointer.cpp

---
+++ LinkedList/NodePointer.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+class Node{
+public:
+    int val;
+    Node* next;
+    Node(int val){
+        this->val = val;
+    }
+};
+int main(){
+    Node* a = new Node(7);  //Head
+    Node* b = new Node(0);
+    Node* c = new Node(1);
+    Node* d = new Node(8);
+    Node* e = new Node(2);
+
+    a->next = b;
+    b->next = c;
+    c->next = d;
+    d->next = e;
+
+    cout << a << endl;
+    cout << a->val << endl;
+    cout << a->next->val << endl;
+    cout << a->next->next->val << endl;
+    cout << a->next->next->next->val << endl;
+    cout << a->next->next->next->next->val << endl;
+}
